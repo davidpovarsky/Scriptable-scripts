6ef227ac97f9d542bb8a1758dff0e2ecea8061f9
davidpovarsky <davidpovarski1@gmail.com>
2025-12-28 20:17:45 +0200

    Update utils.js

--- maps/utils.js
+++ maps/utils.js
@@ -28,7 +28,9 @@ module.exports.fetchJson = async function(url) {
 };
 
 module.exports.sleep = function(ms) {
-  return new Promise((resolve) => Timer.schedule(ms / 1000, false, resolve));
+  return new Promise((resolve) => {
+    Timer.schedule(ms, false, resolve);
+  });
 };
 
 module.exports.loadFallbackLocation = async function () {
